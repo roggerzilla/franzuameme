8c5ff21ab7df36854b8f831edc8ac09eb04427c9
roggerzilla <jose.cabello.moreno98@gmail.com>
2025-01-12 14:49:26 -0600

    correciones

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -2,12 +2,13 @@ import { Routes } from '@angular/router';
 import {FotosComponent} from '../componentes/fotos/fotos.component';
 import {HomeComponent} from '../componentes/home/home.component';
 import {AcercaComponent} from '../componentes/acerca/acerca.component';
+import {ContactoComponent} from '../componentes/contacto/contacto.component';
 
 export const routes: Routes = [
     { path: 'fotos', component: FotosComponent },
     { path: 'home', component: HomeComponent },
     { path: 'acerca', component: AcercaComponent },
-
+    { path: 'contacto', component: ContactoComponent },
     
     { path: '**', component: HomeComponent },
 ];
--- src/componentes/acerca/acerca.component.ts
+++ src/componentes/acerca/acerca.component.ts
@@ -1,4 +1,5 @@
-import { Component } from '@angular/core';
+import { AfterViewInit, Component } from '@angular/core';
+import * as AOS from 'aos'
 
 @Component({
   selector: 'app-acerca',
@@ -6,6 +7,10 @@ import { Component } from '@angular/core';
   templateUrl: './acerca.component.html',
   styleUrl: './acerca.component.css'
 })
-export class AcercaComponent {
-
+export class AcercaComponent implements AfterViewInit{
+  ngAfterViewInit() {
+    if (typeof window !== 'undefined') {
+      AOS.init();
+    }
+  }
 }
